82d4833b796f9fcf1a2943804533a13a737e6229
diankurnia <diankurniaputri5@gmail.com>
2014-10-01 08:46:17 +0700

    add mahasiswa

--- xml/mahasiswa/koneksi.php
+++ xml/mahasiswa/koneksi.php
@@ -1,6 +1,5 @@
 <?php
 	//koneksi database
-	$link = mysql_connect('localhost','root','')
-	or die('Could not connect: '.mysql_error());
-	mysql_select_db('data_mahasiswa')or die('Could not select database');
+	$link = mysql_connect('localhost','root','') or die('Could not connect: '.mysql_error());
+	mysql_select_db('mahasiswa')or die('Could not select database');
 ?>
